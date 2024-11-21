920721af714c43a314e1fd8e05540f59096add6a
Mathews Araujo <mathews.mw@gmail.com>
2024-11-20 20:01:38 -0400

    update aws credentials

--- src/env.ts
+++ src/env.ts
@@ -4,16 +4,16 @@ import { createEnv } from '@t3-oss/env-nextjs';
 export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string(),
-		AWS_ACCESS_KEY: z.string(),
-		AWS_SECRET_KEY: z.string(),
+		AWS_ACCESS_KEY_ID: z.string(),
+		AWS_SECRET_ACCESS_KEY: z.string(),
 		AWS_REGION: z.string(),
 		AWS_BUCKET_NAME: z.string(),
 	},
 
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
-		AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-		AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+		AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+		AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
 		AWS_REGION: process.env.AWS_REGION,
 		AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
 	},
